0ccad37cf7f09270679b178dc3416f9c8cd9fb92
k2 <kkvs1805@gmail.com>
2026-01-27 22:37:17 +0800

    added account class

---
+++ src/main/java/main/Account.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author k2
+ */
+public class Account {
+    private String name;
+    private String type;
+    private double value;
+    
+    Account(String name, String type, double value) {
+        this.name = name;
+        this.type = type;
+        this.value = value;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public String getType() {
+        return this.type;
+    }
+    
+    public double getValue() {
+        return this.value;
+    }
+}
